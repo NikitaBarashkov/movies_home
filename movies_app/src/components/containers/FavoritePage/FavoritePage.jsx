3574e52da7f6b283fce35a39bd8a0664e380f9ec
Nikita Barashkov <nik96barashkov@yandex.ru>
2023-03-12 21:25:35 +0300

    feat: добавил setLocStorageMiddleware для сохранения в localeStorage данных и создал страницу History

---
+++ movies_app/src/components/containers/FavoritePage/FavoritePage.jsx
@@ -0,0 +1,5 @@
+import s from './FavoritePage.module.css';
+
+export const FavoritePage = () => {
+  return <div>FAVORITE</div>;
+};
--- movies_app/src/components/containers/SearchPage/SearchPage.jsx
+++ movies_app/src/components/containers/SearchPage/SearchPage.jsx
@@ -3,60 +3,68 @@ import cn from 'classnames';
 
 import { useTheme } from '../../../hooks/useTheme';
 import { useSearchGoal } from '../../../hooks/useSearchGoal';
+import { useUser } from '../../../hooks/useUser';
 import { CardMovie } from '../../CardMovie/CardMovie';
 import { SearchForm } from '../../SearchForm/SearchForm';
 import { SearchOptionsForm } from '../../SearchOptionsForm/SearchOptionsForm';
 import { MOVIES_API_URL } from '../../../utilities/constants';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addOnHistory } from '../../../store/historySlice';
 
 import s from './SearchPage.module.css';
 
 export const SearchPage = () => {
-  const {isLight} = useTheme();  
- 
-  const [
-    searchGoal, 
-    changeTitleSearchGoal, 
-    changeTypeSearchGoal
-  ] = useSearchGoal();
+  const { isLight } = useTheme();
+  const { user: currentUser } = useUser();
+  const history = useSelector((store) => store.history.history);
 
-  const [responseMovies, setResponseMovies] = useState(null); 
+  const [searchGoal, changeTitleSearchGoal, changeTypeSearchGoal] =
+    useSearchGoal();
+
+  const [responseMovies, setResponseMovies] = useState(null);
   const [fullDataMovie, setFullDataMovie] = useState();
 
   const dispatch = useDispatch();
 
   const fetchMovies = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     let url = MOVIES_API_URL;
-    
+
     if (searchGoal) {
-      url+= `&s=${searchGoal.titleMovie}`;
+      url += `&s=${searchGoal.titleMovie}`;
     }
 
     if (searchGoal.typeMovie !== 'both') {
       url += `&t=${searchGoal.typeMovie}`;
     }
-    
+
     const resp = await fetch(url);
-    const data = await resp.json();  
+    const data = await resp.json();
     setResponseMovies(data);
 
-    if (data.Response === 'True') {
-      dispatch(addOnHistory({search: searchGoal, result: data.Search}))
-    } 
-  }
+    if (data.Response === 'True' && currentUser) {
+      dispatch(
+        addOnHistory({
+          username: currentUser,
+          search: searchGoal.titleMovie,
+          type: searchGoal.typeMovie,
+          link: url,
+          time: new Date().toLocaleString(),
+        })
+      );
+    }
+  };
 
-  const fetchFullInfoMovie = async(title) => {
+  const fetchFullInfoMovie = async (title) => {
     const resp = await fetch(`${MOVIES_API_URL}&t=${title}`);
     const data = await resp.json();
-    console.log(data)
+    console.log(data);
     setFullDataMovie(data);
-  }
+  };
 
   return (
-    <main className={ cn(s.main, {[s.main__light]: isLight}) }>
+    <main className={cn(s.main, { [s.main__light]: isLight })}>
       <section className={s.search_section}>
         <div className={s.radio_container}>
           <h2 className={s.title}>
@@ -64,29 +72,28 @@ export const SearchPage = () => {
           </h2>
           <SearchOptionsForm changeSearchType={changeTypeSearchGoal} />
         </div>
-        <SearchForm 
-          value={searchGoal.titleMovie}   
-          fetchMovies={fetchMovies} 
-          changeValue={changeTitleSearchGoal} 
+        <SearchForm
+          value={searchGoal.titleMovie}
+          fetchMovies={fetchMovies}
+          changeValue={changeTitleSearchGoal}
           theme={isLight}
         />
       </section>
       <section className={s.response_section}>
-        { !responseMovies 
-          ? null
-          : responseMovies.Response === 'True'
-          ? responseMovies.Search.map(movie => 
-            <CardMovie 
-              key={movie.imdbID} 
-              title={movie.Title} 
-              url={movie.Poster} 
+        {!responseMovies ? null : responseMovies.Response === 'True' ? (
+          responseMovies.Search.map((movie) => (
+            <CardMovie
+              key={movie.imdbID}
+              title={movie.Title}
+              url={movie.Poster}
               setInfoMovie={fetchFullInfoMovie}
             />
-            )
-          : <h2 className={s.title__not_found}>
-              Nothing was found for your request
-            </h2>
-        }
+          ))
+        ) : (
+          <h2 className={s.title__not_found}>
+            Nothing was found for your request
+          </h2>
+        )}
       </section>
     </main>
   );
